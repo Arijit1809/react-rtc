2eb0348edf169baf44aa8b8c735ee2c128e8143e
Arijit Das <adas7626@gmail.com>
2025-01-12 12:36:27 +0530

    changed url to hosted socket url

--- src/Socket.ts
+++ src/Socket.ts
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 //TODO: Should be initiated from environment variables
-// const url = "https://game-server-z3yz.onrender.com"; // Replace with your server URL if different
-const url = "http://localhost:8000";	// Replace with your server URL if different
+const url = "https://game-server-z3yz.onrender.com"; // Replace with your server URL if different
+// const url = "http://localhost:8000";	// Replace with your server URL if different
 
 const socket = io(url); // Replace with your server URL if different
 
